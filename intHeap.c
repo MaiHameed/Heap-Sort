27d2b948daf7392c419aafacd968954c69810cef
Mai Hameed <maihameed@hotmail.com>
2018-04-05 16:32:53 -0400

    Initial

--- intHeap.c
+++ intHeap.c
@@ -34,6 +34,49 @@ void addHeap(int thing2add)
 	heap[size-1]=thing2add;
 }
 
+
+void swapHeap(int a, int b){
+	fprintf(stderr, "swap heap[%d] with heap[%d]\n", a, b);
+	int temp = heap[a];
+	heap[a] = heap[b];
+	heap[b] = temp;
+}
+
+
+void printHeap(){
+	int i;
+	fprintf(stderr, "HEAP ARRAY\n");
+	for(i = 0; i<size; i++){
+		fprintf(stderr, "heap[%d]=%d\n", i, heap[i]);
+	}
+	fprintf(stderr, "\n");	
+}
+
+
+void maxHeapify(int parent)
+{	
+	fprintf(stderr, "maxHeapify(%d)\n", parent);
+	int l = (2*parent)+1;
+	int r = (2*parent)+2;
+	int max = parent;
+	int level = 0;
+	while(l < size && r < size){	
+		if(heap[l] > heap[max]){
+			max = l;
+		}
+		if(heap[r] > heap[max]){
+			max = r;
+		}
+		if(max != parent){
+			swapHeap(max, parent);
+			level++;
+			fprintf(stderr, "level = %d parent = %d\n\n",level, parent);
+			maxHeapify(max);
+		}
+	}
+	fprintf(stderr, "Reached end of maxHeapify()\n");
+}
+
 /**
  * heapSize() returns the number of items in the Heap.
  *
--- main.c
+++ main.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 extern int pop();
 extern void push(int);
 extern void addHeap(int);
 extern int heapSize();
+extern void maxHeapify(int); 
+extern void printHeap();
 
 int main(int argc, char * argv[])
 {
-  int value;
+  int value, start, i;
   while (scanf("%d\n", &value) != EOF) {
 	//fprintf(stderr, "READING INPUT: %d\n", value);
     	addHeap(value);
   }
+	printHeap();
+	start = (heapSize()-1)/2;
+	fprintf(stderr, "heapSize() = %d\n", heapSize());
+	fprintf(stderr, "start = %d\n", start);
+	for(i = start; i>0;i--){
+		fprintf(stderr, "i = %d\n", i);
+		maxHeapify(i);
+	}	
 	fprintf(stderr, "heapSize() = %d\n", heapSize());	
   exit(0);
 }
